1b9d4040e09f292d66b30e6f53b3b448d63b5a7b
sistemasIMCG <sistemas@institutomirim.org.br>
2022-02-12 21:20:10 -0400

    update criado com suscesso

---
+++ src/main/java/teste/basico/AlterarUsuario1.java
@@ -0,0 +1,32 @@
+package teste.basico;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import modelo.basico.Usuario;
+
+public class AlterarUsuario1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("exercicios-jpa");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+		
+		Usuario usuario = entityManager.find(Usuario.class, 3L);
+		
+		if(usuario instanceof Usuario) {
+			usuario.setEmail("jotajota@gmail.com"); 
+			}
+		
+		entityManager.merge(usuario);
+		entityManager.getTransaction().commit();
+		
+		entityManager.close();
+		entityManagerFactory.close();
+
+	}
+
+}
